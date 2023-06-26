3c6836be5af5cc676f94dad53498907591aa6e39
Nyaruko166 <occho1666@gmail.com>
2023-06-27 04:54:09 +0700

    Done x2

--- src/main/java/Nyaruko166/Main.java
+++ src/main/java/Nyaruko166/Main.java
@@ -1,16 +1,6 @@
 package Nyaruko166;
 
 import Nyaruko166.view.MainView;
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
 
